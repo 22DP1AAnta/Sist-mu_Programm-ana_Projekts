03ef0063e4c0497b7e065169ea8429e5635c7d10
Artjoms Antanovics <103423056+22DP1AAnta@users.noreply.github.com>
2023-05-21 17:22:35 +0300

    Delete klienti.py

--- klienti.py
+++
@@ -1,9 +0,0 @@
-class Customers:  # definejam klasi
-    def __init__(self, name, age, phone, address):
-        self.name = name
-        self.age = age
-        self.phone = phone
-        self.address = address
-
-    def __str__(self):
-        return f"Name: {self.name}, Age: {self.age}, Phone: {self.phone}, Address: {self.address}"
